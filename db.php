44c55ef6ec67aad9ebba26c04d51f84b9a7eb0fe
Roman <058romanpoudel@gmail.com>
2026-04-06 12:08:52 +0545

    connect to database

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host     = 'localhost';
+$dbname   = 'helpdesk_db';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    http_response_code(500);
+    die(json_encode(['success' => false, 'message' => 'Database connection failed']));
+}
+?>
